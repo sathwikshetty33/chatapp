05ccb3c3f08e7fe699e3d065a5fef70519c2a2b2
Sathwik Shetty <sathwikshetty9876@gmail.com>
2024-12-11 13:21:04 +0530

    added online count

--- django-starter-main/a_rtchat/consumers.py
+++ django-starter-main/a_rtchat/consumers.py
@@ -14,9 +14,13 @@ class ChatroomConsumer(WebsocketConsumer):
         self.user = self.scope['user']
         self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name']
         self.chatroom = get_object_or_404(ChatGroup,group_name=self.chatroom_name)
+
         async_to_sync(self.channel_layer.group_add)(
             self.chatroom_name, self.channel_name
         )
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
         self.accept()
 
     def receive(self, text_data=None, bytes_data=None):
@@ -49,3 +53,22 @@ class ChatroomConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name,self.channel_name
         )
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
+
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count() -1
+
+        event = {
+            'type' : 'online_users_updater',
+            'online_count' : online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+    def online_users_updater(self, event):
+        online_count = event['online_count']
+        html = render_to_string("a_rtchat/partials/online_count.html", {'online' : online_count})
+        self.send(text_data=html)
+
